9089749e39c4030e05131585ba47aa869f4fea19
Jacque0 <jacqueline.cmessias@gmail.com>
2022-04-28 16:33:34 -0300

    projeto gatibook

---
+++ gatitobook/src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+
+@NgModule({
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
+  exports: [HomeComponent],
+})
+export class HomeModule {}
